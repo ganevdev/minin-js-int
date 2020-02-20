96fa9f4d51ba939e101f7ad99f23302dc88ff2f5
Ivan Ganev <iganevru@gmail.com>
2020-02-21 00:51:47 +0300

    07_iife

---
+++ src/07_iife.js
@@ -0,0 +1,24 @@
+// Immediate Invoked Function Expression
+
+// let result = [];
+// for (let i = 0; i < 5; i++) {
+//   result.push(function() {
+//     console.log(i);
+//   });
+// }
+
+// result[2]();
+// result[4]();
+
+let result = [];
+for (var i = 0; i < 5; i++) {
+  (function() {
+    var j = i;
+    result.push(function() {
+      console.log(j);
+    });
+  })();
+}
+
+result[2]();
+result[4]();
